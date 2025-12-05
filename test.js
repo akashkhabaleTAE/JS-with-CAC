8ee9699aebc3dafe3b74578cc84d86d94cdfdb45
akashkhabaleTAE <akashkhabale75@gmail.com>
2025-12-05 21:30:05 +0530

    Reorganize: Move all JS files from 01_basics/ subfolder to root directory

---
+++ test.js
@@ -0,0 +1,14 @@
+const accountId = 163233;
+let accountEmail = "ak@gmail.com";
+var accountPassword = "123";
+let accountCity = "shirala";
+let accountState = "MH";
+
+// accountId = 2; // not allowed
+
+accountEmail = "akashtae@kjei.com";
+accountPassword = "163233";
+accountCity = "pune"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
